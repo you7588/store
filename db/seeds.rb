de802ff5cacc63b3ffdc59b5275056ecfba70ec2
you7588 <yo7588@gmail.com>
2017-08-23 17:38:55 +0800

    add is_admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+u = User.new
+u.email = "123@123.com"
+u.password = "123123"
+u.password_confirmation = "123123"
+u.is_admin = true
+u.save
+
+u = User.new
+u.email = "234@234.com"
+u.password = "123123"
+u.password_confirmation = "123123"
+u.is_admin = true
+u.save
